9fe620e72b3cea1599fd49f69c7c5427d757814d
Valden92 <valden92@gmail.com>
2022-01-25 19:13:26 +0700

    Переписаны некоторые функции и исправлены ошибки

--- user_worker/work_with_user.py
+++ user_worker/work_with_user.py
@@ -1,7 +1,8 @@
 import os
-import requests
+import sys
 from api_worker.work_with_api import VkApiGet
 from generate_report.generate_report import FileSaver
+
 
 param = ['first_name', 'last_name', 'country', 'city', 'bdate', 'sex']
 user_per_page = 500
@@ -13,15 +14,15 @@ def run_program():
     print("Добро пожаловать в VK-api parser.\n"
           "Если захотите прервать выполнение программы, введите 'Q'.")
 
+    # Будет спрашивать авторизационный токен и ID пока пользователь не введет корректный или не выйдет из программы.
     while True:
         token = input('Введите аторизационный токен для VK-api: ').strip()
-        if token.lower() == 'q':
-            break
+        if_q_exit(token)
 
         user_id = input('Введите ID пользователя для извлечения данных: ').strip()
-        if user_id.lower() == 'q':
-            break
+        if_q_exit(user_id)
 
+        # Тут проверяет токен и ID на правильность, а так же проверяет возможность получения данных из VK api.
         try:
             get_api, total_user = run_vk_api(user_id, token, param)
         except ValueError:
@@ -29,26 +30,23 @@ def run_program():
         except (ConnectionError, TypeError):
             break
 
+        # Выводит общеее найденное количество подписчиков для ID.
         if total_user > 0:
             print('Для ID: {} обнаружено {} пользователей.\n'.format(user_id, total_user))
         else:
-            key = no_users()
-            if key == 'q':
-                break
+            no_users(user_id)
 
+        # Запрашивает у пользователя формат отчета.
         file_format = input_format()
-        if file_format == 'q':
-            break
-        else:
-            print('Файл будет сохранен в {} формате.\n'.format(file_format.upper()))
+        print('Файл будет сохранен в {} формате.\n'.format(file_format.upper()))
 
+        # Запрашивает у пользователя путь для сохранения отчета.
         report = input_path(file_format)
-        if report == 'q':
-            break
 
         print('\nВсе необходимые данные получены. Извлечение данных запущено.\n'
               'Ожидайте результатов...')
 
+        # Запускает извлечение данных.
         try:
             report.run(total_user=total_user, get_from_api=get_api)
         except Exception as e:
@@ -62,6 +60,16 @@ def run_program():
 
 
 def run_vk_api(user_id, token, parameters):
+    """Создает экземпляр класса VkApiGet и проверят наличие ошибок в вводе данных, а так же соединение с Vk api
+    и наличие ошибок в ответе Vk api.
+    :param user_id: ID пользователя, по которому делается запрос с целью извлечения его подписчиков;
+    :param token: ваш токен (токен приложения) с которого выполняется запрос к VK api;
+    :param parameters: параметры для передачи их в поле fields VK api, формирующие конечным список данных
+    об одном пользователе.
+    :except ValueError: если введены значения, содержащие неверный тип или сами по себе неверного типа;
+    :except ConnectionError: если наблюдаются проблемы с соединением VK api или наличие ошибок в ответе и т.д.
+    :return: экземпляр класса VkApiGet и общее количество пользователей для запрошенного ID.
+    """
     try:
         api = VkApiGet(user_id=user_id, access_token=token, parameters=parameters)
     except ValueError as e:
@@ -70,63 +78,79 @@ def run_vk_api(user_id, token, parameters):
     except ConnectionError as e:
         print(e)
         raise ConnectionError
-    except requests.exceptions.ConnectionError:
-        print('Ошибка соединения. Установить соединение не удалось.')
-        raise ConnectionError
     else:
         return api, api.users_count
 
 
-def no_users():
-    print('Для ID:{} подписчиков не обнаружено.')
+def no_users(user_id):
+    """Работает, если у ID не обнаружено подписчиков.
+    Предлагает либо начать заново вводить все данные, для нового запроса, либо завершить работу с программой.
+    :param user_id: ID пользователя, по которому делается запрос с целью извлечения его подписчиков.
+    """
+    print('Для ID:{} подписчиков не обнаружено.'.format(user_id))
     while True:
         key = input("Желаете начать заново (Y) или прекратить выполнение программы (Q): ").strip().lower()
-        if key == 'y' or key == 'q':
-            return key
+        if_q_exit(key)
+        if key == 'y':
+            break
         else:
             print('Введенное значение не распознано. Попробуйте еще раз.')
             continue
 
 
+def if_q_exit(string):
+    """Завершает работу приложения, если передаваемый параметр равен 'Q'.
+    :param string: параметр типа str.
+    """
+    if string.lower() == 'q':
+        sys.exit()
+
+
 def input_format():
+    """Получает формат отчета для сохранения от пользователя.
+    :return: формат отчета в виде строки ('csv', 'tsv', 'json').
+    """
     msg = 'Впишите цифру, соответствующую формату (1 - CSV, 2 - TSV, 3 - JSON) для сохранения отчета или ' \
           'нажми "Enter", тогда автоматически будет выбран CSV формат: '
     while True:
         key = input(msg).strip().lower()
+        if_q_exit(key)
         if key == "1" or not key:
             return 'csv'
         elif key == "2":
             return 'tsv'
         elif key == "3":
             return 'json'
-        elif key == 'q':
-            return 'q'
         else:
             print('Введенное значение не распознано. Попробуйте еще раз.')
             continue
 
 
 def input_path(file_format):
-    msg = 'Введите путь для сохранения отчета или напишите название новой директории.\n' \
+    """Запрашивает у пользователя путь для сохранения отчета и создает экземпляр FileSaver.
+    :param file_format: формат отчета в виде строки ('csv', 'tsv', 'json').
+    :except (SyntaxError, FileNotFoundError): выбрасывает, если указан невозможный путь для сохранения.
+    :return: экземпляр FileSaver.
+    """
+    msg = 'Введите путь для сохранения отчета в виде (C:/Users/...) или напишите название новой директории.\n' \
           'Если такой директории не сущестует, то программа попытается ее создать.\n' \
           'Также вы можете просто нажать "Enter" и файл сохранится в одной директории с программой.'
     print(msg)
     while True:
         path = input('Укажите путь для сохранения отчета: ').strip()
-        if path.lower() == 'q':
-            return path.lower()
-        else:
-            if not path:
-                path = os.getcwd()
+        if_q_exit(path)
 
-            try:
-                report = FileSaver(path=path, parameters=param, file_format=file_format, users_per_page=user_per_page)
-            except Exception as e:
-                print(e)
-                print('Пробуйте ввести путь еще раз или введите "Q" для завершения программы.')
-                continue
-            else:
-                return report
+        if not path:
+            path = os.getcwd()
+
+        try:
+            report = FileSaver(path=path, parameters=param, file_format=file_format, users_per_page=user_per_page)
+        except (SyntaxError, FileNotFoundError, OSError) as e:
+            print(e)
+            print('Пробуйте ввести путь еще раз или введите "Q" для завершения программы.')
+            continue
+        else:
+            return report
 
 
 if __name__ == '__main__':
